682668159a0ded17939617cd9fb9be05b4140706
Oriana Mendez <egleemendez@gmail.com>
2024-07-15 13:24:03 +0200

    import AddPatient component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { AddPatient } from "./components/add-patient";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>PhysioReact</h1>
+      <AddPatient />
+    </>
+  );
 }
 
 export default App;
